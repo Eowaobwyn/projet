5b7b06c6c23ac62fae453cb6448b17dffd154d2f
corentin.dieval <corentin.dieval@gmail.com>
2018-03-17 06:19:33 -0400

    Ajout de test plus ajout de la gem Nominatim

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,2 +1,16 @@
 class Town < ActiveRecord::Base
+  before_validation :set_geocode
+  validates :name, :latitude, :longitude, presence: true
+
+  
+ private  
+  def set_geocode 
+    towns = Nominatim.search.city(self.name).limit(1)
+    if towns && towns.first
+      current_town = towns.first
+      #self.zipcode = current_town.zipcode
+      self.latitude = current_town.lat
+      self.longitude = current_town.lon
+    end
+  end
 end
--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -4,4 +4,16 @@ class TownTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "town validation does geocoding" do
+    belfort = Town.new
+    belfort.name = 'belfort'
+    assert belfort.valid?
+    assert_equal(47.6379599, belfort.latitude)
+    assert_equal(6.8628942, belfort.longitude)
+  end
+  test "Town does not exist is not valid" do
+    Unknown = Town.new
+    Unknown.name = 'MyString'
+    assert !Unknown.valid?
+  end 
 end
